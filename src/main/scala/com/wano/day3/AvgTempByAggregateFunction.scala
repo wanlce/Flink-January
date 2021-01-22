4e1d20d717ec9d1c9a60c91aa8b013c05838ad14
亦朴 <whox@foxmail.com>
2021-01-22 15:53:03 +0800

    update

--- src/main/scala/com/wano/day3/AvgTempByAggregateFunction.scala
+++ src/main/scala/com/wano/day3/AvgTempByAggregateFunction.scala
@@ -3,12 +3,17 @@ package com.wano.day3
 import com.wano.day2.{SensorReading, SensorSource}
 import org.apache.flink.api.common.functions.AggregateFunction
 import org.apache.flink.streaming.api.scala._
+import org.apache.flink.streaming.api.scala.function.ProcessWindowFunction
 import org.apache.flink.streaming.api.windowing.time.Time
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow
+import org.apache.flink.util.Collector
 
 /**
  * 增量窗口聚合
  */
 object AvgTempByAggregateFunction {
+  case class AvgInfo(id: String, avgTemp: Double, windowStart: Long, windowEnd: Long)
+
   def main(args: Array[String]): Unit = {
 
     val env =  StreamExecutionEnvironment.getExecutionEnvironment
@@ -18,7 +23,7 @@ object AvgTempByAggregateFunction {
 
     stream.keyBy(_.id)
       .timeWindow(Time.seconds(5))
-      .aggregate(new AvgTempAgg)
+      .aggregate(new AvgTempAgg, new WindowResult)
       .print()
 
     env.execute()
@@ -42,6 +47,14 @@ object AvgTempByAggregateFunction {
 
     override def merge(a: (String, Long, Double), b: (String, Long, Double)): (String, Long, Double) = {
       (a._1, a._2 + b._2, a._3 + b._3)
+    }
+  }
+  // 输入的泛型是增量聚合函数的输出类型
+  class WindowResult extends ProcessWindowFunction[(String, Double), AvgInfo, String, TimeWindow] {
+    override def process(key: String, context: Context, elements: Iterable[(String, Double)], out: Collector[AvgInfo]): Unit = {
+      // 迭代器中只有一个值，就是增量聚合函数发送过来的聚合结果
+      out.collect(AvgInfo(key, elements.head._2, context.window.getStart, context.window.getEnd))
+
     }
   }
 
